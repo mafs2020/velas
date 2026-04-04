721319c3ac483ff8ac4af9e1d28b30b65053e064
martin117h <martin117h@gmail.com>
2026-04-03 20:20:32 -0600

    se tipa el indicadoe de la ema

--- src/app/core/indicadores/ema.ts
+++ src/app/core/indicadores/ema.ts
@@ -1,15 +1,16 @@
+import { LineData, Time, UTCTimestamp } from 'lightweight-charts';
 import { Candle } from '../inicio/servicios/prueba';
 
-export function calculateEMA(data: Candle[], period: number): any[] {
+export function calculateEMA(data: Candle[], period: number): LineData<Time>[] {
   const k = 2 / (period + 1);
   let emaPrev = data[0].close;
 
-  return data.map((c) => {
+  return data.map((c, i) => {
     const ema = c.close * k + emaPrev * (1 - k);
     emaPrev = ema;
 
     return {
-      time: c.time,
+      time: c.time as UTCTimestamp,
       value: ema,
     };
   });
